633e397912b7d2cd630c7d4e564b4fb965a9ed6a
Mika Turunen <mika.turunen@ymail.com>
2015-05-01 10:46:20 +0300

    Minor refactor for easier reading in main.js

--- main.js
+++ main.js
@@ -20,7 +20,8 @@ app.on("window-all-closed", function() {
 app.on("ready", function() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
-        width: 800, height: 600
+        width: 800,
+        height: 600
     });
 
     // and load the index.html of the app.
